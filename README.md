b461765f2babd26d794ee9a8431b15f9e07c83cd
julian cardona <juliancardona07@gmail.com>
2021-12-06 10:35:03 -0500

    cambios

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # curso-practico-javascript
+
 curso practico de javascript
+
+## Taller 1: Figuras Geometricas
+
+-primer paso: definir las formulas
+-segundo paso: implementar las formulas en javascript
+-tercer paso: crear funciones
+-cuarto paso: integrar javascript con html.
---
+++ figurasfunciones.js
@@ -0,0 +1,64 @@
+////////////////CODIGO CUADRADO///////////////
+//creacion de variables
+console.group("CUADRADO");
+//const ladoCuadrado = 5;
+//imprimir texto
+console.log(`los lados del cuadrado miden: ${ladoCuadrado} cm`);
+//creando el perimetro
+const perimetroCuadrado = ladoCuadrado * 4;
+//imprmiendo el perimetro
+console.log(`el perimetro del cuadrado mide: ${perimetroCuadrado} cm`);
+//creando el area
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+//imprmiendo el area
+console.log(`el area del cuadrado mide: ${areaCuadrado} cm2`);
+console.groupEnd();
+
+function perimetroCuadrado2(lado) {
+  return ladoCuadrado * 4;
+}
+
+////////////////CODIGO TRIANGULO///////////////
+//creacion de variables
+console.group("TRIANGULO");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+//imprimir texto
+console.log(
+  `los lados del triangulo miden: ${ladoTriangulo1} cm, 
+  ${ladoTriangulo2} cm, ${baseTriangulo} cm `
+);
+//imprimiendo la altura
+console.log(`la altura del triangulo es ${alturaTriangulo} cm`);
+
+//creando el perimetro
+//const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+function perimetroTriangulo(lado1, lado2, base) {
+  return ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+}
+//imprmiendo el perimetro
+console.log(`el perimetro del triangulo mide: ${perimetroTriangulo} cm`);
+//creando el area
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+//imprmiendo el area
+console.log(`el area del triangulo mide: ${areaTriangulo} cm2`);
+console.groupEnd();
+
+////////////////CODIGO CIRCULO
+console.group("CIRCULO");
+//creacion variables
+const radioCirculo = 4;
+const diametroCirculo = radioCirculo * 2;
+const PI = Math.PI;
+const perimetroCirculo = diametroCirculo * PI;
+const areaCirculo = radioCirculo * radioCirculo * PI;
+
+console.log(`el radio del circulo es ${radioCirculo} cm`);
+console.log(`el diametro del circulo es ${diametroCirculo} cm`);
+console.log(`el PI del circulo es ${PI} cm`);
+console.log(`el perimetro del circulo es ${perimetroCirculo} cm`);
+console.log(`el area del circulo es ${areaCirculo} cm2`);
+
+console.groupEnd();
